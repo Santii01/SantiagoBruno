acb8a6e63487c3913332622a53af2bdd3e2a0960
Santii <santiagobruno865@gmail.com>
2021-08-18 21:27:38 -0300

    Tpform

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-const edadTp = (nombre, apellido, edad) => {
-    if(edad <= 17){
-        console.log(nombre, apellido, "Tienes menos de 18, sal de aqui")
-    }else if (edad >= 18 && edad <= 59){
-        console.log(nombre, apellido, "Pasa crack, eres adulto")
-    }else if(edad >= 60){
-        console.log(nombre, apellido, "Pasa pero ten cuidado ya tienes 60")
-    }
+const formF = event=> {
+    event.preventDefault()
+    const formState = []
+    const formatoMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    
+    let name = document.getElementById("nombre")
+    let apellido = document.getElementById("lastname")
+    let edad = document.getElementById("age")
+    let email = document.getElementById("email")
+    let comments = document.getElementById("comments")
+    let checkbox = document.getElementById("checkboxx")
+    
+
+    !name.value ? name.classList.add("error") :
+    formState.push({ element: 'nombre', value: name.value}) && name.classList.remove('error')
+
+    !apellido.value ? apellido.classList.add("error") :
+    formState.push({ element: 'lastname', value: apellido.value}) && name.classList.remove("error")
+
+    !edad.value ? edad.classList.add("error") :
+    formState.push ({ element: 'age', value: edad.value}) && name.classList.remove("error")
+
+    !email.value || !email.value.match(formatoMail) ? email.classList.add("error") :
+    formState.push({ element: 'email', value: email.value}) && name.classList.remove("error")
+
+    !comments.value ? comments.classList.add("error") :
+    formState.push({ element: 'comments', value: comments.value}) && name.classList.remove("error")
+
+    const dato = document.getElementById("formavalues")
+    const html = formState.map(item => {
+        let h2 = document.createElement('h2')
+        h2.innerText = item.value
+        return dato.appendChild(h2)
+
+    })
+
+
+    formState.length === 3 && (dato.innerHTML = JSON.stringify(html))
+
 }
-console.log(edadTp("Raul", "Salazar", 60))
+
